f06f7edc83bdc001234984edba6e96f59de0332d
Hasan4413 <hasannasird59@gmail.com>
2023-05-06 00:15:10 +0500

    prodHome completed without responsive

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './global.styles.scss'
-import Footer from './Components/Footer'
-import MainPage from './Pages/mainPage'
-import StateHome from './Pages/stateHome'
+import ProdHome from './Pages/prodHome'
+// import Footer from './Components/Footer'
+// import MainPage from './Pages/mainPage'
+// import StateHome from './Pages/stateHome'
+// import prodHome from './Pages/prodHome'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,7 +15,8 @@ function App() {
     <div>
       {/* <MainPage/>
       <Footer/> */}
-     <StateHome/>
+     {/* <StateHome/> */}
+     <ProdHome/>
     </div>
   )
 }
---
+++ src/Pages/prodHome.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import '../Components/Button'
+import ButtonMain from "../Components/Button";
+function ProdHome() {
+  return (
+    <div className="prod-main">
+       <div className="buttons-parent">
+        <ButtonMain name="What we do"/>
+        <ButtonMain name="Press"/>
+        <ButtonMain name="Our Studios"/>
+        <ButtonMain name="The Team"/>
+        <ButtonMain name="Company"/>
+        <ButtonMain name="Our Films"/>
+        <ButtonMain name="Profile"/>
+        <ButtonMain name="Site Map"/>
+        </div>
+
+
+
+
+    </div>
+  )
+}
+
+export default ProdHome;
