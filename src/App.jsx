8032fa06b20dd91dab6000521dfa665e29008622
Macbook <macbook@MacBook-Pro.local>
2023-05-06 00:17:32 +0500

    home page & routing integrated

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './global.styles.scss'
-import Footer from './Components/Footer'
-import MainPage from './Pages/mainPage'
-import StateHome from './Pages/stateHome'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./global.styles.scss";
+import Footer from "./Components/Footer";
+import MainPage from "./Pages/mainPage";
+import StateHome from "./Pages/stateHome";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div>
-      {/* <MainPage/>
-      <Footer/> */}
+      <Routes>
+        <Route path="/" Component={MainPage} />
+        <Route path="/State" Component={StateHome}/>
+      </Routes>
+
+      {/* 
+      {/* <Footer/> 
      <StateHome/>
+     */}
     </div>
-  )
+  );
 }
 
-export default App
-   
+export default App;
